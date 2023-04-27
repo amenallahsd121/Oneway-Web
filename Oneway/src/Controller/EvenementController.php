506e2b8480211a4e6d017a37fb97dd4cbfb5b43a
Meddebsofien <sofien.meddeb@esprit.tn>
2023-04-27 01:37:01 +0100

    searche+pagination+qrcode+calendar

--- Oneway/src/Controller/EvenementController.php
+++ Oneway/src/Controller/EvenementController.php
@@ -8,6 +8,7 @@ use App\Entity\Evenement;
 use App\Form\EvenementType;
 use App\Repository\EvenementRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\OpportinuteRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -124,7 +125,78 @@ public function delete($id) {
   }
 
   
+  #[Route('/calendar/event', name: 'app_evenement_calendar')]
+  public function calendarEvent(EvenementRepository $evenment, OpportinuteRepository $opp): Response
+  {
+      $events= $evenment->findAll();
+      $ops= $opp->findAll();
+      $rdvs = [];
+      $rdvs1 = [];
+      foreach($events as $event){
+        $rdvs[] = [
+          //  'id' => $event->getIdEvent(),
+            'start' => $event->getDateDebutEvent()->format('Y-m-d'),
+            'end' => $event->getDateFinEvent()->format('Y-m-d'),
+            'title' => $event->getNom(),
+            
+             'color' => 'red', 
+             'borderColor' => 'bleu',
+             'textColor' => 'bleu',
+             'backgroundColor' => 'bleu',
+         
+        ];
+    }
+    foreach($ops as $o){
+        $rdvs1[] = [
+          
+            'start' => $o->getDate()->format('Y-m-d'),
+            
+            'title' => $o->getDepart(),
+            
+            'color' => 'red',
+            'url' => '../affectation/delete/'.$o->getIdOpp() .' '
+         
+        ];
+    }
+
+
+    $data = json_encode($rdvs);
+    $dataop = json_encode($rdvs1);
+      
     
+      return $this->render('\evenement\calendar.html.twig', compact('data','dataop'));
+  }
+  #[Route("/participation/search", name:"participation_search")]
+        public function search(Request $request , EntityManagerInterface $entityManager,PaginatorInterface $paginator )
+        {   
+           
+
+            $searchQuery = $request->query->get('q');
+
+            //$em = $this->getDoctrine()->getManager();
+
+            $queryBuilder = $entityManager->createQueryBuilder()
+                ->select('e')
+                ->from(Evenement::class, 'e')
+                ->where('e.nom LIKE :query')
+                ->orWhere('e.description LIKE :query')
+                ->orWhere('e.awards LIKE :query')
+                ->setParameter('query', '%'.$searchQuery.'%');
+            
+            $list = $queryBuilder->getQuery()->getResult();
+
+         
+                $pagination = $paginator->paginate( $list , $request->query->getInt('page', 1 ), 3);  
+
+            return $this->render('participation/search.html.twig', [
+               // 'offre' =>$pagination,
+                'list' => $pagination,
+                'search_query' => $searchQuery
+                
+            ]);
+        }
+
+
 
 
 }
--- Oneway/src/Repository/EvenementRepository.php
+++ Oneway/src/Repository/EvenementRepository.php
@@ -39,28 +39,45 @@ class EvenementRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Evenement[] Returns an array of Evenement objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   /**
+    * @return Evenement[] Returns an array of Evenement objects
+    */
+   public function findByExampleField($value): array
+   {
+       return $this->createQueryBuilder('e')
+           ->andWhere('e.exampleField = :val')
+           ->setParameter('val', $value)
+           ->orderBy('e.id', 'ASC')
+           ->setMaxResults(10)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
-//    public function findOneBySomeField($value): ?Evenement
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+   public function findOneBySomeField($value): ?Evenement
+   {
+       return $this->createQueryBuilder('e')
+           ->andWhere('e.exampleField = :val')
+           ->setParameter('val', $value)
+           ->getQuery()
+           ->getOneOrNullResult()
+       ;
+   }
+
+
+   public function searchNom($nom)
+        {
+            return $this->createQueryBuilder('s')
+                ->andWhere('s.nom LIKE :ncl')
+                ->setParameter('ncl', $nom.'%')
+                ->getQuery()
+                ->execute();
+        }
+
+        public function findAll(): array
+{
+    return $this->createQueryBuilder('e')
+        ->getQuery()
+        ->getResult();
+}
 }
